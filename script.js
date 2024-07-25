a623ddf3ab13d6c1e9f01b8364a726d6240d5b62
Dorin Bamniya <123871946+Dorinishere@users.noreply.github.com>
2024-07-25 14:23:13 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-const button = document.getElementById('search-button');
-const input = document.getElementById('city-input');
-const cityname = document.getElementById('city-name');
-const citytime = document.getElementById('city-time');
-const citytemp = document.getElementById('city-temp');
-
-async function getData(cityname) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=4d9383adf5e9433b8fa44520242507&q=${cityname}&aqi=no`);
-    return await response.json();
+const button =document.getElementById("stopbutton")
+function showTime() {
+    const currentTime = new Date();
+   const time=`${currentTime.getHours()}: ${currentTime.getMinutes()}:${currentTime.getSeconds()}`
+   document.getElementById('time').innerText=time;
 }
 
-button.addEventListener('click', async () => {
-    const value = input.value;
-    const result = await getData(value);
-    cityname.innerText = `${result.location.name}, ${result.location.region}, ${result.location.country}`;
-    citytime.innerText = result.location.localtime;
-    citytemp.innerText = `${result.current.temp_c}°C`;
-});
+let interval = setInterval(showTime,1000);
 
-
+button.addEventListener('click',()=>{
+    clearInterval(interval);
+})
